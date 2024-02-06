845f96a8191a46c6fe4110735b5dd10bd113622a
RodriMPC <rodriperezcolman@gmail.com>
2024-02-06 15:29:52 -0300

    Commit React

---
+++ src/components/ItemCount/ItemCount.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react"
+
+const ItemCount = () => {
+    const [count, setCount] = useState(0)
+
+   const increment = () => {
+    setCount(count + 1)
+   }
+
+   const decrement = () => {
+    setCount(count - 1)
+   }
+
+   return (
+        <div>
+            <h1>{count}</h1>
+            <button onClick={increment}>Incrementar</button>
+            <button onClick={decrement}>Decrementar</button>
+        </div>
+   ) 
+}
+
+export default ItemCount
